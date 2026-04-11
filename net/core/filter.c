0bb90bb8df5d794226872ec14863fe0767717c1b
Chara <bionichuman07@gmail.com>
2026-04-10 20:47:34 -0400

    Revert "BACKPORT: bpf: Reject narrower access to pointer ctx fields"
    This reverts commit baf5e30467d4634f04f90e18a07b89896a0994cf.

--- net/core/filter.c
+++ net/core/filter.c
@@ -7616,7 +7616,7 @@ static bool bpf_skb_is_valid_access(int off, int size, enum bpf_access_type type
 		if (size != sizeof(__u64))
 			return false;
 		break;
-	case bpf_ctx_range_ptr(struct __sk_buff, sk):
+	case offsetof(struct __sk_buff, sk):
 		if (type == BPF_WRITE || size != sizeof(__u64))
 			return false;
 		info->reg_type = PTR_TO_SOCK_COMMON_OR_NULL;
@@ -8133,11 +8133,7 @@ static bool sock_addr_is_valid_access(int off, int size,
 				return false;
 		}
 		break;
-	case bpf_ctx_range(struct bpf_sock_addr, user_port):
-		if (size != size_default)
-			return false;
-		break;
-	case bpf_ctx_range_ptr(struct bpf_sock_addr, sk):
+	case offsetof(struct bpf_sock_addr, sk):
 		if (type != BPF_READ)
 			return false;
 		if (size != sizeof(__u64))
@@ -8191,17 +8187,17 @@ static bool sock_ops_is_valid_access(int off, int size,
 			if (size != sizeof(__u64))
 				return false;
 			break;
-		case bpf_ctx_range_ptr(struct bpf_sock_ops, sk):
+		case offsetof(struct bpf_sock_ops, sk):
 			if (size != sizeof(__u64))
 				return false;
 			info->reg_type = PTR_TO_SOCKET_OR_NULL;
 			break;
-		case bpf_ctx_range_ptr(struct bpf_sock_ops, skb_data):
+		case offsetof(struct bpf_sock_ops, skb_data):
 			if (size != sizeof(__u64))
 				return false;
 			info->reg_type = PTR_TO_PACKET;
 			break;
-		case bpf_ctx_range_ptr(struct bpf_sock_ops, skb_data_end):
+		case offsetof(struct bpf_sock_ops, skb_data_end):
 			if (size != sizeof(__u64))
 				return false;
 			info->reg_type = PTR_TO_PACKET_END;
@@ -8275,17 +8271,17 @@ static bool sk_msg_is_valid_access(int off, int size,
 		return false;
 
 	switch (off) {
-	case bpf_ctx_range_ptr(struct sk_msg_md, data):
+	case offsetof(struct sk_msg_md, data):
 		info->reg_type = PTR_TO_PACKET;
 		if (size != sizeof(__u64))
 			return false;
 		break;
-	case bpf_ctx_range_ptr(struct sk_msg_md, data_end):
+	case offsetof(struct sk_msg_md, data_end):
 		info->reg_type = PTR_TO_PACKET_END;
 		if (size != sizeof(__u64))
 			return false;
 		break;
-	case bpf_ctx_range_ptr(struct sk_msg_md, sk):
+	case offsetof(struct sk_msg_md, sk):
 		if (size != sizeof(__u64))
 			return false;
 		info->reg_type = PTR_TO_SOCKET;
@@ -10310,7 +10306,7 @@ static bool sk_lookup_is_valid_access(int off, int size,
 		return false;
 
 	switch (off) {
-	case bpf_ctx_range_ptr(struct bpf_sk_lookup, sk):
+	case offsetof(struct bpf_sk_lookup, sk):
 		info->reg_type = PTR_TO_SOCKET_OR_NULL;
 		return size == sizeof(__u64);
 
@@ -10417,7 +10413,6 @@ static u32 sk_lookup_convert_ctx_access(enum bpf_access_type type,
 }
 
 const struct bpf_prog_ops sk_lookup_prog_ops = {
-	.test_run = bpf_prog_test_run_sk_lookup,
 };
 
 const struct bpf_verifier_ops sk_lookup_verifier_ops = {
